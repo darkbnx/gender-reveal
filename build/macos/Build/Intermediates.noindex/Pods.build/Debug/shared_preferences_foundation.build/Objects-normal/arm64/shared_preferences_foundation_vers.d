59ceaf2f7627d6d4a203e79894e1bd5be09c2efb
marco <marcorego1994@gmail.com>
2025-09-26 11:09:15 +0200

    Deploy nova versão web

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/Objects-normal/arm64/shared_preferences_foundation_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/marcocascalheira/Desktop/Escola/Tese\ de\ Mestrado/Gender\ Reveal/flutter_application_1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/module.modulemap \
+  /Users/marcocascalheira/Desktop/Escola/Tese\ de\ Mestrado/Gender\ Reveal/flutter_application_1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/DerivedSources/shared_preferences_foundation_vers.c
